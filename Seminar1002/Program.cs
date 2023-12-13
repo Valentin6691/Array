2a1236b5d85a5825974d29b5e884b46fe077c597
Valentin <nitnelav6691@gmail.cov>
2023-12-13 16:02:58 +0300

    seinar1002

---
+++ Seminar1002/Program.cs
@@ -0,0 +1,13 @@
+﻿        int[] array = new int[10];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(-10, 11);
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = array[i] * -1;
+            Console.Write(array[i] + " ");
+        }

70964271176ff9ca1d6dd65efb8be3f98db920dc
Valentin <nitnelav6691@gmail.cov>
2023-12-13 13:27:26 +0300

    seinar1001

---
+++ Seminar1001/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = new int[12];
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool isFinde = false;
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,4);
+    Console.Write(array[i] + " ");
+
+    if(array[i] == num)
+    {
+        isFinde = true;
+    }
+    
+}
+
+if(isFinde == true)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+     Console.WriteLine("Нет ");
+}

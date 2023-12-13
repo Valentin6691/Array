4ce03e7afbde52184aeb76c484462770a5f2931c
Valentin <nitnelav6691@gmail.cov>
2023-12-13 13:44:12 +0300

    seinar1001

--- Seminar1001/Program.cs
+++ Seminar1001/Program.cs
@@ -1,27 +1,32 @@
-﻿int[] array = new int[12];
-
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
-
-bool isFinde = false;
-
-for(int i = 0; i < array.Length; i++)
+﻿internal class Program
 {
-    array[i] = new Random().Next(0,4);
-    Console.Write(array[i] + " ");
-
-    if(array[i] == num)
+    private static void Main(string[] args)
     {
-        isFinde = true;
-    }
-    
-}
+        Console.WriteLine("Введите число");
 
-if(isFinde == true)
-{
-    Console.WriteLine("Да");
-}
-else
-{
-     Console.WriteLine("Нет ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int array[] = new int[5];
+        bool isFind = false;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(0, 14);
+            Console.Write(array(i) + " ");
+
+            if (array(i) == num)
+            {
+                isFind = true;
+                break;
+            }
+        }
+        if (isFind == true)
+        {
+            Console.Write("Да" + num);
+        }
+        else
+        {
+            Console.Write("Нет");
+        }
+    }
 }

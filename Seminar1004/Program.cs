54b22ecb01ac6354e2b6c42e79f08083e25b7aa7
Valentin <nitnelav6691@gmail.cov>
2023-12-13 20:22:37 +0300

    seinar1004

---
+++ Seminar1004/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int [] array = new int[3]; 
+        if(num>99 && num<1000)
+        {
+array[0] = num%10;
+array[1] = num%100/10;
+array[2] = num/100;
+for(int i = 0; i<array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+        }
+        else
+        {
+            Console.WriteLine("Число не подходит");
+        }
+
+    }
+}

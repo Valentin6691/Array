8eaef7b3754310de423ecd984b9729591d63a1af
Valentin <nitnelav6691@gmail.cov>
2023-12-13 19:31:05 +0300

    seinar1003

---
+++ Seminar1003/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int[num];
+int [] array2 = new int[num/2];
+
+for(int i = 0; i<array.Length; i++)
+{
+array[i] = new Random().Next(-20, 30);
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for(int i = 0; i<array2.Length; i++)
+{
+array2[i] = array[i] * array[array.Length - 1 - i];
+Console.Write(array2[i] + " ");
+}

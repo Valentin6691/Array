8600e257814ffa8c7a76ffb6f912534916fbee49
Valentin <nitnelav6691@gmail.cov>
2023-12-14 08:54:12 +0300

    seinar1005

---
+++ Seminar1005/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[10];
+int num = 0;
+for(int i = 0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(1,101);
+    Console.Write(array[i] + " ");
+    if(array[i] > 19  && array[i] < 92)
+    {
+num = num + 1;
+    }
+}
+Console.WriteLine();
+Console.Write(num);
